040339ffcf3038ad9e7f76d1e825aa8ca25c135e
MadinaAbd <98716107+MadinaAbd@users.noreply.github.com>
2022-07-18 19:23:30 +0300

    Внесены изменения в таблицу questions

--- db/migrate/20220716192213_create_categories.rb
+++ db/migrate/20220716192213_create_categories.rb
@@ -1,3 +1,4 @@
+
 class CreateCategories < ActiveRecord::Migration[6.1]
   def change
     create_table :categories do |t|
--- db/schema.rb
+++ db/schema.rb
@@ -29,11 +29,10 @@ ActiveRecord::Schema.define(version: 2022_07_16_192213) do
 
   create_table "questions", force: :cascade do |t|
     t.string "body", null: false
-    t.integer "tests_id", null: false
-    t.integer "level_id", null: false
+    t.integer "test_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["level_id"], name: "index_questions_on_level_id"
+    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "tests", force: :cascade do |t|
@@ -54,7 +53,7 @@ ActiveRecord::Schema.define(version: 2022_07_16_192213) do
   end
 
   add_foreign_key "answers", "questions"
-  add_foreign_key "questions", "levels"
+  add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
   add_foreign_key "tests", "users", column: "author_id"
 end
