894e8ce29812718e466a48026a0dcf8a860e1184
MadinaAbd <98716107+MadinaAbd@users.noreply.github.com>
2022-07-27 21:14:33 +0300

    Внесены правки

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,9 @@
 class Test < ApplicationRecord
 
   def self.tests_by_cotegory(category_title)
-    joins(:category_id)
+    joins('JOIN categories ON tests.category_id = categories.id')
     .where(categories: {title: category_title})
-    .order(title: desc)
+    .order(title: :desc)
     .pluck(:title)
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -27,23 +27,23 @@ categories = Category.create!([
 
 
 tests = Test.create!([
-  {title: 'Ruby', category_id: categories[0], author_id: user[0], level: '1'},
-  {title: 'JavaScript', category_id: categories[1], author_id: user[1], level: '1'},
-  {title: 'Swift', category_id: categories[2], author_id: user[2], level: '1'}
+  {title: 'Ruby', category_id: categories[0].id, author_id: users[0].id, level: '1'},
+  {title: 'JavaScript', category_id: categories[1].id, author_id: users[1].id, level: '1'},
+  {title: 'Swift', category_id: categories[2].id, author_id: users[2].id, level: '1'}
 ])
 
 
 questions = Qustion.create!([
-  {body: 'Когда появился язык программирования Ruby?',test_id: tests[0]},
-  {body: 'Когда появился язык программирования JavaScript?',test_id: tests[1]},
-  {body: 'Когда появился язык программирования Swift?',test_id: tests[2]}
+  {body: 'Когда появился язык программирования Ruby?',test_id: tests[0].id},
+  {body: 'Когда появился язык программирования JavaScript?',test_id: tests[1].id},
+  {body: 'Когда появился язык программирования Swift?',test_id: tests[2].id}
 ])
 
-answers = Answer.create!([
-  {title: '1995', correct: true, question_id: questions[0]},
-  {title: '1996', correct: false, question_id: questions[0]},
-  {title: '1995', correct: true, question_id: questions[1]},
-  {title: '1998', correct: false, question_id: questions[1]},
-  {title: '2014', correct: true, question_id: questions[2]},
-  {title: '2013', correct: false, question_id: questions[2]}
-])
+# answers = Answer.create!([
+#   {title: '1995', correct: true, question_id: questions[0]},
+#   {title: '1996', correct: false, question_id: questions[0]},
+#   {title: '1995', correct: true, question_id: questions[1]},
+#   {title: '1998', correct: false, question_id: questions[1]},
+#   {title: '2014', correct: true, question_id: questions[2]},
+#   {title: '2013', correct: false, question_id: questions[2]}
+# ])
