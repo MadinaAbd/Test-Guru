52c55ec4dee9285f6bd9ab194fdce34d57669953
MadinaAbd <98716107+MadinaAbd@users.noreply.github.com>
2022-07-30 17:45:56 +0300

    Внесены изменения согласно дз к 5му уроку

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,8 @@
 class Test < ApplicationRecord
+  has_many :questions
+  belongs_to :category
+  has_many :users, trought: :user_tests
+  belongs_to :author, class_name: 'User'
 
   def self.tests_by_cotegory(category_title)
     joins('JOIN categories ON tests.category_id = categories.id')
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :tests, trought: :user_tests
+  has_many :author, class_name: 'Test'
 
   def level_test(level)
     Test.joins('JOIN user_tests ON user_tests.test_id = tests.id')
@@ -9,6 +11,3 @@ class User < ApplicationRecord
 end
 
 
-
-# Создайте инстанс-метод в модели User, который принимает в качестве аргумента значение уровня сложности и
-# возвращает список всех Тестов, которые проходит или когда-либо проходил Пользователь на этом уровне сложности.
