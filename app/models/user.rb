75455efee52a7cd46b44762cc4fb391b2c1aea94
MadinaAbd <98716107+MadinaAbd@users.noreply.github.com>
2022-08-01 23:01:44 +0300

    Внесены правки согласно замечаниям

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  has_many :tests, through: :user_tests
-  has_many :author, class_name: 'Test'
+  has_many :user_tests, dependent: :destroy
+  has_many :tests, through: :user_tests, dependent: :destroy
+  has_many :author, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
 
   def level_test(level)
     Test.joins('JOIN user_tests ON user_tests.test_id = tests.id')
