1879deb8ce4b808ffd4dff32164afe3a6cc5c55f
MadinaAbd <98716107+MadinaAbd@users.noreply.github.com>
2022-08-08 20:00:32 +0300

    Дз к 6 уроку

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,15 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
+  scope :correct, -> { where(correct: true) }
+
+  validate :validate_answer_options, on :create
+
+  private
+
+  def validate_answer_options
+    errors.add(:base) if question.answers.size > 4
+  end
+
 end
+
