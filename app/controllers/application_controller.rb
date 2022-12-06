df853127dbea09a6805da7fe397e3b37c599b6aa
MadinaAbd <98716107+MadinaAbd@users.noreply.github.com>
2022-12-06 22:54:53 +0300

    ДЗ к 12 уроку

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
- before_action :configure_permitted_parameters, if: :devise_controller?
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
+  def default_url_options
+    { lang: I18n.locale }
+  end
 
   private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
@@ -10,6 +20,5 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(_resource)
     current_user.is_a?(Admin) ? admin_tests_path : tests_path
   end
-
 
 end
