7120c2f98633e6d48e2352d999186ec434b93c59
MadinaAbd <98716107+MadinaAbd@users.noreply.github.com>
2022-12-08 22:14:16 +0300

    Twitter Bootstrap 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,24 @@
-class UsersController < ApplicationController
+# class UsersController < ApplicationController
 
-  def new
-    @user = User.new
-  end
+#   def new
+#     @user = User.new
+#   end
 
-  def create
-    @user = User.new(user_params)
+#   def create
+#     @user = User.new(user_params)
 
-    if @user.save
-      redirect_to tests_path
-    else
-      render :new
-    end
+#     if @user.save
+#       redirect_to tests_path
+#     else
+#       render :new
+#     end
 
-  end
+#   end
 
-  private
+#   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+#   def user_params
+#     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+#   end
 
-end
+# end
